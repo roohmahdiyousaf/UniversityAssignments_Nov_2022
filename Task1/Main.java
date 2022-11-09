e126fe9b5dd424783ae5808eff42f1a18ba8c1cd
roohmahdiyousaf <117854671+roohmahdiyousaf@users.noreply.github.com>
2022-11-09 23:23:23 +0500

    Update Main.java

--- Task1/Main.java
+++ Task1/Main.java
@@ -1,1 +1,17 @@
-// Hi
+class M_Rooh
+{
+  public static void main(String[] args)
+  {
+    int a = 1;
+    float b = 1.0f;
+    double c = 123.12367d;
+    String e = "Muhammad Rooh e Mehdi";
+    boolean f = true;
+    
+    System.out.println("Integer Value is: " + a);
+    System.out.println("Float Value is: " + b);
+    System.out.println("Double Value is: " + c);
+    System.out.println("String Value is: " + e);
+    System.out.println("Boolean Value is: " + f);
+  }
+}
