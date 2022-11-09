92bb44086aad94e621b8eb35fe9724dbf891f64e
roohmahdiyousaf <117854671+roohmahdiyousaf@users.noreply.github.com>
2022-11-09 23:26:54 +0500

    Create Main.java

---
+++ Task2/Main.java
@@ -0,0 +1,17 @@
+class M_Rooh
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a Number: ");
+    
+    int a = sc.nextInt();
+    
+    if(a%2== 0)
+    {
+      System.out.println("The value of a is an even number.");
+    }else{
+      System.out.println("The value of a is an odd number.");
+    }
+  }
+}
