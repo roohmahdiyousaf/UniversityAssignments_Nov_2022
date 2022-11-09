4f1666ba5ab296ac816ad7b6ee9948fdee294f9a
roohmahdiyousaf <117854671+roohmahdiyousaf@users.noreply.github.com>
2022-11-09 23:33:57 +0500

    Create Main.java

---
+++ Task3/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class M_Rooh
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    
+    int a = 2;
+    int b = 3;
+    
+    System.out.println("Addition of a and b: " + (a + b));
+    System.out.println("Subtraction of a and b: " + (a - b));
+    System.out.println("Multiplication of a and b: " + (a * b));
+    System.out.println("Division of a and b: " + (a / b));
+    System.out.println("Remainder of a and b: " + (a % b));
+    
+  }
+}
